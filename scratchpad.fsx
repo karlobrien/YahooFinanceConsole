d4671f1beb6b1d1ab549ad253b812652dc0ffe27
karlobrien <karlobrien@gmail.com>
2016-08-04 21:27:33 +0100

    interactive file

---
+++ scratchpad.fsx
@@ -0,0 +1,18 @@
+#load "CommonLibrary.fs"
+#load "AsyncDownloader.fs"
+
+open Library.CommonLibrary
+open Library.Downloader
+
+let name = "ebay"
+let ebayUrl = "http://ichart.finance.yahoo.com/table.csv?s=EBAY&d=6&e=6&f=2013&g=d&a=1&b=1&c=2010&ignore=.csv"
+
+let urlList = [name, ebayUrl]
+
+let prices = urlList |> Seq.map fetchWithAsync
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> Seq.collect htmlToMarketData
+
+let maxClose2 = prices |> Seq.maxBy (fun m -> m.Close)
+printfn "%f" maxClose2.Close 
